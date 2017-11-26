fe7b3ff0113616e9e662d3ccb339e80d9a4a848b
Jim Pick <jim@backblend.com>
2017-11-25 23:08:54 -0800

    Add beginning of testrig with tape
    Runs mkcontainer to build the test image.

---
+++ test-single-subnet-mdns.js
@@ -0,0 +1,21 @@
+var test = require('tape')
+// var cp = require('child_process')
+var execspawn = require('npm-execspawn')
+// var spawn = require('tape-spawn')
+
+test('local network, 2 nodes, dat share and dat clone, mdns', function (t) {
+  buildImage(function (err) {
+    if (err) t.end(err)
+  })
+  t.end()
+})
+
+function buildImage (cb) {
+  var mc = execspawn('mkcontainer')
+  mc.stderr.pipe(process.stderr)
+  mc.stdout.pipe(process.stdout)
+  mc.on('close', function (code) {
+    if (code) return cb(new Error('mkcontainer failed'))
+    cb()
+  })
+}
